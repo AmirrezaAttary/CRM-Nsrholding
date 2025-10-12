8f6302c9a306fa31b3937c4dfb6db9c93daa1c9e
Amiraliarzande <amiraliarzande8@gmail.com>
2025-10-12 18:52:44 +0330

    Starting the CargoAnnouncement model

--- core/app/crm/admin.py
+++ core/app/crm/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.db.models import Q
-from .models import CallReport, FieldActivity, ValidationLevel
+from .models import CallReport, FieldActivity, ValidationLevel, CargoAnnouncement, ProductType, PortName, CountryName, LoadingTime
 from django_jalali.admin.filters import JDateFieldListFilter
 from django_jalali.admin.widgets import AdminjDateWidget
+
+######################
+# admin panel Call Report
 
 class FieldActivityAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent')  
@@ -26,6 +29,110 @@ class ValidationLevelAdmin(admin.ModelAdmin):
     list_filter = ('parent',)          
     ordering = ('parent', 'name') 
 
+##################################
+
+# Original Admin Panel Cargo Announcement
+
+class CargoAnnouncementAdmin(admin.ModelAdmin):
+    list_display = ("load_type", "display_name", "product_type","port_name","country_name","loading_time")
+    list_filter = ("load_type", "product_type","port_name","country_name","loading_time")
+    search_fields = ("full_name", "name_company", "name_ceo","port_name","country_name","loading_time")
+    ordering = ("load_type", "product_type")
+
+    fieldsets = (
+        (_("Load Type"), {
+            "fields": ("load_type",)
+        }),
+        (_("Personal Information"), {
+            "fields": ("full_name", "number"),
+            "classes": ("collapse",)
+        }),
+        (_("Company Information"), {
+            "fields": ("name_company", "name_ceo", "number_ceo"),
+            "classes": ("collapse",)
+        }),
+        (_("Brand Name Information"), {
+            "fields": ("product_type",)
+        }),
+        (_("Port Name Information"), {
+            "fields": ("port_name",)
+        }),
+        (_("Country Name Information"), {
+            "fields": ("country_name",)
+        }),
+        (_("Loading Time Information"), {
+            "fields": ("loading_time",)
+        }),
+    )
+
+    def display_name(self, obj):
+        if obj.load_type == obj.LoadType.COMPANY and obj.name_company:
+            return obj.name_company
+        return obj.full_name or "-"
+    display_name.short_description = _("Name")
+
+# admin panel Product Type
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent")
+    list_filter = ("parent",)
+    search_fields = ("name",)
+    ordering = ("parent__name", "name")
+    verbose_name = _("Product Type")
+
+    fieldsets = (
+        (_("Product Type Information"), {
+            "fields": ("name", "parent")
+        }),
+    )
+
+# admin panel Port Name
+class PortNameAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent")
+    list_filter = ("parent",)
+    search_fields = ("name",)
+    ordering = ("parent__name", "name")
+    verbose_name = _("Port Name")
+
+    fieldsets = (
+        (_("Port Name Information"), {
+            "fields": ("name", "parent")
+        }),
+    )
+
+# admin panel Country Name
+class CountryNameAdmin (admin.ModelAdmin):
+    list_display = ("name", "parent")
+    list_filter = ("parent",)
+    search_fields = ("name",)
+    ordering = ("parent__name", "name")
+    verbose_name = _("Port Name")
+
+    fieldsets = (
+        (_("Country Name Information"), {
+            "fields": ("name", "parent")
+        }),
+    )
+
+
+# admin panel Loading Time
+class LoadingTimeAdmin (admin.ModelAdmin):
+    list_display = ("name", "parent")
+    list_filter = ("parent",)
+    search_fields = ("name",)
+    ordering = ("parent__name", "name")
+    verbose_name = _("Port Name")
+
+    fieldsets = (
+        (_("Loading Time Information"), {
+            "fields": ("name", "parent")
+        }),
+    )
+
 admin.site.register(FieldActivity, FieldActivityAdmin)
 admin.site.register(CallReport, CallReportAdmin)
 admin.site.register(ValidationLevel,ValidationLevelAdmin)
+admin.site.register(ProductType,ProductTypeAdmin)
+admin.site.register(CargoAnnouncement,CargoAnnouncementAdmin)
+admin.site.register(PortName,PortNameAdmin)
+admin.site.register(CountryName,CountryNameAdmin)
+admin.site.register(LoadingTime,LoadingTimeAdmin)
--- core/app/crm/models.py
+++ core/app/crm/models.py
@@ -91,4 +91,160 @@ class CallReport (models.Model):
     def __str__(self):
         return f"{self.number or 'No Number'} - {self.name or 'Unnamed'}"
     
+####################################################################################
+# All Model Cargo Announcement
+
+class ProductType (models.Model):
+
+    name = models.CharField(max_length=100, verbose_name=_("Product Type Name"))
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name=_("Parent Product Type")
+    )
+
+    class Meta:
+        verbose_name = _("Product Type Level")
+        verbose_name_plural = _("Product Type Levels")
+
+    def __str__(self):
+        if self.parent:
+            return f"{self.parent.name} > {self.name}"
+        return self.name
     
+# model PortName
+class PortName (models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Port Name"))
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name=_("Parent Port Name")
+    )
+
+    class Meta:
+        verbose_name = _("Port Name Level")
+        verbose_name_plural = _("Port Name Levels")
+
+    def __str__(self):
+        if self.parent:
+            return f"{self.parent.name} > {self.name}"
+        return self.name
+
+# model CountryName
+class CountryName (models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Country Name"))
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name=_("Parent Country Name")
+    )
+
+    class Meta:
+        verbose_name = _("Country Name Level")
+        verbose_name_plural = _("Country Name Levels")
+
+    def __str__(self):
+        if self.parent:
+            return f"{self.parent.name} > {self.name}"
+        return self.name
+    
+class LoadingTime (models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Loading Time"))
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name=_("Parent Loading Time")
+    )
+
+    class Meta:
+        verbose_name = _("Loading Time Level")
+        verbose_name_plural = _("Loading Time Levels")
+
+    def __str__(self):
+        if self.parent:
+            return f"{self.parent.name} > {self.name}"
+        return self.name
+
+    
+# Original Model Cargo Announcement
+class CargoAnnouncement (models.Model):
+
+    class LoadType(models.TextChoices):
+        PERSONAL = 'personal', _("Personal")
+        COMPANY = 'company', _("Company")
+
+    load_type = models.CharField(
+        max_length=20,
+        choices=LoadType.choices,
+        default=LoadType.PERSONAL,
+        verbose_name=_("Load Type")
+    )
+
+    full_name = models.CharField(max_length=100, verbose_name=_("full name"), blank=True, null=True)
+    number = models.IntegerField(verbose_name=_("number"), blank=True, null=True)
+
+    name_company = models.CharField (max_length=100, verbose_name=_("name company"), blank=True, null=True)
+    name_ceo =  models.CharField (max_length=100, verbose_name=_("name ceo"), blank=True, null=True)
+    number_ceo = models.CharField (max_length=100, verbose_name=_("number ceo"), blank=True, null=True)
+
+
+
+    product_type = models.ForeignKey(
+    ProductType,
+    on_delete=models.PROTECT,  
+    null=True,
+    blank=True,
+    verbose_name=_("Product Type"),
+    related_name='CargoAnnouncement'
+    )
+
+    port_name = models.ForeignKey(
+    PortName,
+    on_delete=models.PROTECT,
+    null=True,
+    blank=True,
+    verbose_name = _("Port Name"),
+    related_name='CargoAnnouncement'
+    )
+
+    country_name = models.ForeignKey(
+    CountryName,
+    on_delete=models.PROTECT,
+    null=True,
+    blank=True,
+    verbose_name = _("Country Name"),
+    related_name='CargoAnnouncement'
+    )
+
+    loading_time = models.ForeignKey(
+    LoadingTime,
+    on_delete=models.PROTECT,
+    null=True,
+    blank=True,
+    verbose_name = _("Loading Time"),
+    related_name='CargoAnnouncement'
+    )
+
+
+
+    class Meta:
+        verbose_name = _("CargoAnnouncement")
+        verbose_name_plural = _("CargoAnnouncement")
+
+    def __str__(self):
+        if self.load_type == self.LoadType.COMPANY and self.name_company:
+            return f"{self.name_company} ({self.name_ceo} → {self.number_ceo})"
+        else:
+            return f"{self.full_name} {self.number}"
